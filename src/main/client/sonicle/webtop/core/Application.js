1e2e5be8772b9fd547e047e9cbb8bfb4648a239e
Gabriele Bulfon <gbulfon@sonicle.com>
2016-02-25 09:32:05 +0100

    ctx menu, e.tag -> e.menuData

--- src/main/client/sonicle/webtop/core/Application.js
+++ src/main/client/sonicle/webtop/core/Application.js
@@ -240,7 +240,7 @@ Ext.override(Ext.data.proxy.Server, {
 
 Ext.override(Ext.menu.Item, {
 	onClick: function(e) {
-		e.tag = WT.getContextMenuData();
+		e.menuData = WT.getContextMenuData();
 		return this.callParent([e]);
 	}
 });
