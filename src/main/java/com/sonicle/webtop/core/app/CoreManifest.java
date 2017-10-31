255ccb64e3a1d9f17986cefbf2c438345fa1a0ad
Gabriele Bulfon <gbulfon@sonicle.com>
2017-10-31 12:17:46 +0100

    === Version 5.1.1 ===

--- src/main/java/com/sonicle/webtop/core/app/CoreManifest.java
+++ src/main/java/com/sonicle/webtop/core/app/CoreManifest.java
@@ -47,8 +47,8 @@ public class CoreManifest extends ServiceManifest {
 	public static final String XID = "wt";
 	public static final String JAVA_PACKAGE = "com.sonicle.webtop.core";
 	public static final String JS_PACKAGE = "Sonicle.webtop.core";
-	public static final String VERSION = "5.0.19";
-	public static final String BUILD_DATE = "2017-09-27";
+	public static final String VERSION = "5.1.1";
+	public static final String BUILD_DATE = "2017-10-31";
 	public static final String CONTROLLER_CLASSNAME = "com.sonicle.webtop.core.CoreController";
 	public static final String MANAGER_CLASSNAME = "com.sonicle.webtop.core.CoreManager";
 	public static final String REST_API_CLASSNAME = "com.sonicle.webtop.core.CoreRestApi";
