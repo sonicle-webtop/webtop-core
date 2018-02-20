4ff66809eda6509d693f35959092949c6a12c129
Matteo Albinola <malbinola@sonicle.com>
2018-02-20 16:29:16 +0100

    === Version 5.1.5 ===

--- src/main/java/com/sonicle/webtop/core/app/CoreAdminManifest.java
+++ src/main/java/com/sonicle/webtop/core/app/CoreAdminManifest.java
@@ -47,8 +47,8 @@ public class CoreAdminManifest extends ServiceManifest {
 	public static final String XID = "wtadm";
 	public static final String JAVA_PACKAGE = "com.sonicle.webtop.core.admin";
 	public static final String JS_PACKAGE = "Sonicle.webtop.core.admin";
-	public static final String VERSION = "5.1.4";
-	public static final String BUILD_DATE = "2018-02-16";
+	public static final String VERSION = "5.1.5";
+	public static final String BUILD_DATE = "2018-02-20";
 	public static final String CONTROLLER_CLASSNAME = "com.sonicle.webtop.core.admin.Controller";
 	public static final String MANAGER_CLASSNAME = "com.sonicle.webtop.core.admin.CoreAdminManager";
 	public static final String PRIVATE_SERVICE_CLASSNAME = "com.sonicle.webtop.core.admin.Service";
