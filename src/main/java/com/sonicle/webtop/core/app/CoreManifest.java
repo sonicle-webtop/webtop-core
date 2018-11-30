9997699ead12deb74c29b116789a6da59d1a9e06
Matteo Albinola <malbinola@sonicle.com>
2018-11-30 15:30:15 +0100

    === Version 5.5.0 ===

--- src/main/java/com/sonicle/webtop/core/app/CoreManifest.java
+++ src/main/java/com/sonicle/webtop/core/app/CoreManifest.java
@@ -47,8 +47,8 @@ public class CoreManifest extends ServiceManifest {
 	public static final String XID = "wt";
 	public static final String JAVA_PACKAGE = "com.sonicle.webtop.core";
 	public static final String JS_PACKAGE = "Sonicle.webtop.core";
-	public static final String VERSION = "5.4.1";
-	public static final String BUILD_DATE = "2018-10-08";
+	public static final String VERSION = "5.5.0";
+	public static final String BUILD_DATE = "2018-11-30";
 	public static final String CONTROLLER_CLASSNAME = "com.sonicle.webtop.core.CoreController";
 	public static final String MANAGER_CLASSNAME = "com.sonicle.webtop.core.CoreManager";
 	public static final String REST_API_CLASSNAME = "com.sonicle.webtop.core.RestApi";
