5a71c460e80f8bfdadbf9ae03f793960060693c2
Matteo Albinola <malbinola@sonicle.com>
2020-02-26 14:42:47 +0100

    Fix typo

--- src/main/resources/com/sonicle/webtop/core/meta/db/9.9.9_1.sql
+++ src/main/resources/com/sonicle/webtop/core/meta/db/9.9.9_1.sql
@@ -124,10 +124,10 @@ ALTER TABLE "core"."custom_panels_fields" ADD FOREIGN KEY ("tag_id") REFERENCES 
 CREATE TABLE "core"."licenses" (
 "internet_domain" varchar(255) NOT NULL,
 "product_id" varchar(255) NOT NULL,
-"license" text,
+"license" text
 )
 WITH (OIDS=FALSE)
 
-;;
+;
 
 ALTER TABLE "core"."licenses" ADD PRIMARY KEY ("internet_domain", "product_id");
