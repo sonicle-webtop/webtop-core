51dbfe10bb6df253c1118c87eb06023be630a6ae
Matteo Albinola <malbinola@sonicle.com>
2021-09-22 10:24:18 +0200

    Add fr language, update locales

---
+++ src/main/resources/com/sonicle/webtop/core/meta/db/5.11.6_1.sql
@@ -0,0 +1,7 @@
+@DataSource[default@com.sonicle.webtop.core]
+
+-- ----------------------------
+-- Add new fr language
+-- ----------------------------
+@IgnoreErrors
+INSERT INTO "core"."languages" ("language_tag") VALUES ('fr_FR');
