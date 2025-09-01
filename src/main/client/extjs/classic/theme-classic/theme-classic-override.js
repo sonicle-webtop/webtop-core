ab2e294751aca02641f1a88de09f5bb516fb9d11
malbinola <malbinola@sonicle.com>
2025-09-01 15:59:46 +0200

    Fix kanbancolumn in dark-theme WT-1427

--- src/main/client/extjs/classic/theme-classic/theme-classic-override.js
+++ src/main/client/extjs/classic/theme-classic/theme-classic-override.js
@@ -4,3 +4,8 @@ Ext.define('Ext.theme.classic.Component', {
 	//Ext.namespace('Ext.theme.ui.button').primary = 'default';
 	Ext.namespace('Ext.theme.ui.button').toolbar = 'default-toolbar';
 });
+Ext.define('Sonicle.overrides.panel.KanbanColumn', {
+	override: 'Sonicle.panel.KanbanColumn',
+	
+	border: true
+});
